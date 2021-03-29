7eaa6ad26fbe4b8079798fcb8a42ba7185f04884
Tobias Kirschstein <tobias.kirschstein@gmail.com>
2021-03-29 20:04:24 +0200

    Add support for pip install

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='code_transformer',
       version='0.1',
       description='Code Transformer',
       author='Daniel Zügner, Tobias Kirschstein, Michele Catasta, Jure Leskovec, Stephan Günnemann',
       author_email='zuegnerd@in.tum.de,kirschto@in.tum.de',
-      packages=['code_transformer'],
+      packages=find_packages(),
       install_requires=['numpy', 'scipy', 'matplotlib', 'torch', 'tqdm',
-                        'jsonlines', 'jsonpickle', 'networkx', 'sacred', 'joblib',
+                        'jsonlines', 'jsonpickle<2.0.0', 'networkx', 'sacred', 'joblib',
                         'pandas', 'transformers', 'rouge', 'Pygments', 'PyYAML', 'environs'],
       zip_safe=False)
